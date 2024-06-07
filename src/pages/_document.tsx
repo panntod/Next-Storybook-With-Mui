21c3da7509da0ca504a50b25c2845c2bf35a3bc7
panntod <pandhu.munjalindra@gmail.com>
2024-06-08 05:33:50 +0700

    feat: Initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentProps,
+  DocumentContext,
+} from 'next/document'
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from '@mui/material-nextjs/v14-pagesRouter'
+import themes, { roboto } from '@/themes'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+export default function MyDocument(props: DocumentProps & DocumentHeadTagsProps) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const theme = React.useMemo(
+    () => (prefersDarkMode ? themes.dark : themes.light),
+    [prefersDarkMode]
+  )
+  return (
+    <Html lang="en" className={roboto.className}>
+      <Head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="emotion-insertion-point" content="" />
+        <DocumentHeadTags {...props} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx)
+  return finalProps
+}
